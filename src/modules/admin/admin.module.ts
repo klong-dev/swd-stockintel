f06583221fae15ed6b9d9ec491a538a606c8e0fa
Hoàng Kim Long <longhoang8204@gmail.com>
2025-07-04 21:50:28 +0700

    fix(Admin): fixed missing jwt provider

--- src/modules/admin/admin.module.ts
+++ src/modules/admin/admin.module.ts
@@ -5,14 +5,17 @@ import { AdminController } from './admin.controller';
 import { Post } from '../post/entities/post.entity';
 import { Report } from '../report/entities/report.entity';
 import { RedisModule } from '../redis/redis.module';
+import { JwtConfigModule } from '../../configs/jwt-config.module';
+import { JwtAuthGuard } from '../../guards/jwt-auth.guard';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Post, Report]),
     RedisModule,
+    JwtConfigModule,
   ],
   controllers: [AdminController],
-  providers: [AdminService],
+  providers: [AdminService, JwtAuthGuard],
   exports: [AdminService],
 })
 export class AdminModule { }
